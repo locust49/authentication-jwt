7427ec44ec359567b58be2efb4af74d0c4b7119e
locust49 <lyazid.souad@gmail.com>
2021-03-31 00:38:10 +0100

    User Model

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,5 @@
+const User = require('../models/User');
+
 module.exports.signup_get = (request, response) => {
 	response.render('signup');
 }
@@ -6,14 +8,20 @@ module.exports.login_get = (request, response) => {
 	response.render('login');
 }
 
-module.exports.signup_post = (request, response) => {
+module.exports.signup_post = async (request, response) => {
 	const { email, password } = request.body;
 
-	console.log(email, password);
-	response.send('new signup');
+	try {
+		const user = await User.create({ email, password });
+		response.status(201).json(user);
+	}
+	catch (error){
+		console.error(error);
+		response.status(400).send('error, user not created')
+	}
 }
 
-module.exports.login_post = (request, response) => {
+module.exports.login_post = async (request, response) => {
 	const { email, password } = request.body;
 
 	console.log(email, password);
