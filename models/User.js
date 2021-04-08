7fe58e250c12eb62a56b38182ce77bf358d54ec9
locust49 <lyazid.souad@gmail.com>
2021-04-08 16:29:06 +0100

    add login static method

--- models/User.js
+++ models/User.js
@@ -18,11 +18,26 @@ const userSchema = new mongoose.Schema({
 });
 
 // fire function before doc saved to db.
-userSchema.pre('save', async function (next) {
+userSchema.pre('save', async function (next)
+{
 	const salt = await bcrypt.genSalt();
 	this.password = await bcrypt.hash(this.password, salt);
 	next();
 })
+
+// static method to login user
+userSchema.statics.login = async function(email, password)
+{
+	const user = await this.findOne({ email });
+	if (user)
+	{
+		const auth = await bcrypt.compare(password, user.password);
+		if (auth)
+			return user;
+		throw Error('incorrect password');
+	}
+	throw Error('incorrect email');
+}
 
 const User = mongoose.model('user', userSchema);
 
